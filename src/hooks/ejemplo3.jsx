0f81e7b0c516bbd7f0a7a83126186b00bb6413b1
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-01-15 18:41:26 +0000

    Add imagen

--- src/hooks/ejemplo3.jsx
+++ src/hooks/ejemplo3.jsx
@@ -1,27 +1,46 @@
-import React, {useState} from 'react'
-
-
+import React, { useState } from "react";
+import Papel from "../IMAGEN/PAPEL.png";
+import Tijera from "../IMAGEN/TIJERA.png";
+import Piedra from "../IMAGEN/PIEDRA.png";
 
 export function Ejemplo3() {
-const estadoIncial = {
-  token: '12345',
-  session: 1
-}
+  const estadoIncial = {
+    card1: "CARTA 1",
+    card2: "CARTA 2",
+  };
 
-const [sessionData, setSessionData] = useState(estadoIncial)
+  const [sessionData, setResult] = useState(estadoIncial);
 
-
-const UpdateSession = ()=> {
-  setSessionData({
-    token: "TOKEN ACTUALIZADO"  ,
-    session: sessionData.session + 1
-  })
-}
+  const UpdateSession = () => {
+    const nombres = [Papel, Tijera, Piedra];
+    const aleatorio1 = nombres[Math.floor(Math.random() * nombres.length)];
+    const aleatorio2 = nombres[Math.floor(Math.random() * nombres.length)];
+    setResult({
+      card1: aleatorio1,
+      card2: aleatorio2,
+    });
+  };
   return (
-    <div>
-    <p>TOKEN: { sessionData.token} </p>
-    <p>SESSION: { sessionData.session}</p> 
-    <button onClick={UpdateSession}> CAMBIAR TOKEN Y SESSION</button>
+    <div class="mainBlock">
+      <div class="column">
+        <div>
+          <p id="jugador">jugador 1</p>
+          <p id="card1">
+            <img id="imagen" src={sessionData.card1}></img>
+          </p>
+        </div>
+        <div>
+          <p id="jugador">jugador 2</p>
+          <p id="card1">
+            <img id="imagen" src={sessionData.card2}></img>
+          </p>
+        </div>
+      </div>
+      <div class="column">
+        <button id="buttonCambiarToken" onClick={UpdateSession}>
+          JUGAR
+        </button>
+      </div>
     </div>
-  )
+  );
 }
