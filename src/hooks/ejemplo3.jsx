87db68ad0b6566435bfe3aa66ee29543d665edd3
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-01-21 15:26:00 +0000

    imagen

--- src/hooks/ejemplo3.jsx
+++ src/hooks/ejemplo3.jsx
@@ -27,13 +27,13 @@ export function Ejemplo3() {
         <div>
           <p id="jugador">JUGADOR 1</p>
           <p id="card1">
-            <img id={sessionData.fix} src={sessionData.card1} alt="description of image"></img>
+            <img id={sessionData.fix} src={sessionData.card1} alt="description "></img>
           </p>
         </div>
         <div>
           <p id="jugador">JUGADOR 2</p>
           <p id="card1">
-            <img id={sessionData.fix} src={sessionData.card2} alt="description of image"></img>
+            <img id={sessionData.fix} src={sessionData.card2} alt="description"></img>
           </p>
         </div>
       </div>
